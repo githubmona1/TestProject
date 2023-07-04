2c3ce62b3db9afda7034c6cbeb551a90906c6ee9
atish-nandeshwar-nivi <atish.nandeshwar@nivisolutions.com>
2023-07-04 18:03:34 +0530

    TestProgram commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// C# program to print Hello World!
+
+// namespace declaration
+namespace HelloWorldApp
+{
+
+    // Class declaration
+    class Geeks
+    {
+
+        // Main Method
+        static void Main(string[] args)
+        {
+
+            // statement
+            // printing Hello World!
+            Console.WriteLine("Hello World!");
+
+            // To prevents the screen from
+            // running and closing quickly
+            Console.ReadKey();
+        }
+    }
+}
